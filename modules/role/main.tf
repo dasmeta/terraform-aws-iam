cf11188110528b88f89f95be6173aa31ad30002e
Aram Karapetyan <aram@dasmeta.com>
2023-10-30 16:14:29 +0400

    feat(DMVP-iam-role): Iam Role

---
+++ modules/role/main.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_policy" "this" {
+  name        = var.name
+  description = var.description
+
+  policy = var.policy
+}
+
+resource "aws_iam_role" "aws-role" {
+  name = var.name
+
+  assume_role_policy = var.trust_policy
+}
+
+resource "aws_iam_role_policy_attachment" "AWSRole" {
+  policy_arn = aws_iam_policy.this.arn
+  role       = aws_iam_role.aws-role.name
+}
---
+++ modules/role/variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+    type = string    
+}
+
+variable "description" {
+    type = string
+}
+
+variable "policy" {
+    type = any
+}
+
+variable "trust_policy" {
+    type = any
+}
