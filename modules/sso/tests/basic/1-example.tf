99e34325db2e4107614763e198450270aed744ef
Aram Karapetyan <aram@dasmeta.com>
2024-09-24 12:16:56 +0400

    feat(DMVP-5181): Add IAM Role module

--- modules/sso/tests/basic/1-example.tf
+++ modules/sso/tests/basic/1-example.tf
@@ -6,6 +6,10 @@ module "this" {
       group  = "Administrators", //Group name
       policy = ["arn:aws:iam::aws:policy/AdministratorAccess"]
     },
+    # {
+    #   group  = "Developers", //Group name
+    #   policy = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
+    # }
     {
       group  = "Developers", //Group name
       policy = ["arn:aws:iam::aws:policy/ReadOnlyAccess"]
