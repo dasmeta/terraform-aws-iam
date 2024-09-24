117204e9d83ff12850d4a42b6808beed5560665a
Aram Karapetyan <aram@dasmeta.com>
2024-09-24 12:16:37 +0400

    feat(DMVP-5181): Add IAM Role module

--- modules/role/variables.tf
+++ modules/role/variables.tf
@@ -1,15 +1,27 @@
 variable "name" {
-    type = string    
+  type        = string
+  description = "Role name"
 }
 
 variable "description" {
-    type = string
+  type        = string
+  description = "Role description"
 }
 
 variable "policy" {
-    type = any
+  type = list(object({
+    actions   = list(string)
+    resources = list(string)
+  }))
+  description = "AWS role assigne policy"
 }
 
-variable "trust_policy" {
-    type = any
+variable "trust_relationship" {
+  type = list(object({
+    effect     = optional(string, "Allow") # Effect of the policy (Allow or Deny)
+    actions    = list(string)              # Actions like sts:AssumeRole
+    principals = any                       # Principals (e.g., AWS, Service, Federated)
+    conditions = optional(any, [])         # Optional conditions for assume role
+  }))
+  description = "AWS role trust relationship"
 }
